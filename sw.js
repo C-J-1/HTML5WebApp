73552e8281b83f42857beb511926f937abc19b91
Christian Jensen <24825148+C-J-1@users.noreply.github.com>
2019-08-23 02:25:02 +1000

    Add Installer

--- sw.js
+++ sw.js
@@ -56,12 +56,6 @@ let deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
-  ...
-});
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
   // Update UI notify the user they can add to home screen
   showInstallPromotion();
 });
