4f1f5c432bddb1e89f1e79c924c5e8cad6c96832
Christian Jensen <24825148+C-J-1@users.noreply.github.com>
2019-08-22 23:51:34 +1000

    Update service worker
    Added css js and image files to cached files using the Workbox service worker and it's "Cache First" strategy.

--- service-worker.js
+++ service-worker.js
@@ -10,28 +10,21 @@ if (workbox) {
 
 workbox.routing.registerRoute(
   /\.js$/,
-  new workbox.strategies.NetworkFirst()
+  new workbox.strategies.CacheFirst({
+    cacheName: 'cached-js',
+  })
 );
 
 workbox.routing.registerRoute(
   /\.css$/,
-  new workbox.strategies.NetworkFirst()
+  new workbox.strategies.CacheFirst({
+    cacheName: 'cached-css',
+  })
 );
 
 workbox.routing.registerRoute(
-  // Cache image files.
   /\.(?:png|jpg|jpeg|svg|gif)$/,
-  // Use the cache if it's available.
   new workbox.strategies.CacheFirst({
-    // Use a custom cache name.
-    cacheName: 'image-cache',
-    plugins: [
-      new workbox.expiration.Plugin({
-        // Cache only 20 images.
-        maxEntries: 20,
-        // Cache for a maximum of a week.
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-      })
-    ],
+    cacheName: 'cached-images',
   })
 );
