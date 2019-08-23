58b48decce7c8e5e3af2dfbcd1d2db25bf0fb487
Christian Jensen <24825148+C-J-1@users.noreply.github.com>
2019-08-23 18:53:36 +1000

    Added install prompt, fixed service worker & manifest
    The WebApp can now be installed to desktop when the user clicks the install button on the index.html page (Firefox tested and working).
    
    Due to PWA limitations, this can seemingly only be done once... ever. Users will need instructions on how to download and install the WebApp on a separate "help" page, if they miss the prompt.

--- sw.js
+++ sw.js
@@ -8,45 +8,31 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-workbox.precaching.precacheAndRoute([
-  {
-    "url": "css/style.css",
-    "revision": "08c24ee4ea6a48f1dd16201b76c589cd"
-  },
-  {
-    "url": "img/box1.png",
-    "revision": "acae9a5a589ec78f323d58990c5e43d0"
-  },
-  {
-    "url": "img/box2.png",
-    "revision": "4037bb8da3472ebf7e369d0d7c163b90"
-  },
-  {
-    "url": "img/box3.png",
-    "revision": "8c74d7236da4cb57283e3fca052253fa"
-  },
-  {
-    "url": "img/showcase1.jpg",
-    "revision": "259dab902f8eb7e9559fd3e6d729034f"
-  },
-  {
-    "url": "index.html",
-    "revision": "fcbf9f053ab9e8e052b21d6f4b07ea50"
-  },
-  {
-    "url": "manifest.json",
-    "revision": "f1c5d2bf7b927e3f15258002fd5ebea2"
-  },
-  {
-    "url": "maths/maths.html",
-    "revision": "b3167d87ef88dd71aa4cdc178d71acac"
-  },
-  {
-    "url": "other/other.html",
-    "revision": "54524599fc79b59fd089b92adc02bcd2"
-  },
-  {
-    "url": "statistics/statistics.html",
-    "revision": "f2da3ab6759965b5271bb4ebbdd9fa75"
-  }
-]);
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       "css/style.css",
+       "img/box1.png",
+       "img/box2.png",
+       "img/box3.png",
+       "img/showcase1.jpg",
+       "index.html",
+       "manifest.json",
+       "maths/maths.html",
+       "other/other.html",
+       "install.js",
+       "statistics/statistics.html",
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
